7873b8ec09ee4adeacf7c33496653b5e402e4e6d
Umid Negmatullayev <undigitals@gmail.com>
2018-10-21 22:55:19 +0900

    refetch query

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
 
 //  Query Import
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 
 
@@ -34,7 +34,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorId: this.state.authorId
-            }
+            },
+            refetchQueries: [{ query: getBooksQuery }]
         });
     }
 
